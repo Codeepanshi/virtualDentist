e48f9689b3528ab656a514eaebe50ef585fc2fcd
Codeepanshi <codenest24@gmail.com>
2025-05-06 17:33:59 -0600

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,8 @@ import Header from './Components/header';
 import Owner from './Components/owner';
 import Form from './Components/form';
 import Footer from './Components/footer';
-import { useState } from 'react';
-import AdminLogin from './admin/adminlogin';
-import PrivateRoute from './Components/privateRoute';
-import AdminPage from './Components/adminPage';
+
 function App() {
-const [token, setToken] = useState(localStorage.getItem('token'));
-
-  const handleSetToken = (token) => {
-    setToken(token);
-    localStorage.setItem('token', token);
-  };
-
   return (
     <Router>
       <div className="App">
@@ -28,15 +18,6 @@ const [token, setToken] = useState(localStorage.getItem('token'));
               <Footer />
             </>
           } />
-          <Route path="/admin/login" element={<AdminLogin setToken={handleSetToken} />} />
-          <Route 
-            path="/admin" 
-            element={
-              <PrivateRoute token={token}>
-                <AdminPage setToken={setToken}/>
-              </PrivateRoute>
-            } 
-          />
         </Routes>
       </div>
     </Router>
