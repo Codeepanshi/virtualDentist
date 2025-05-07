16fd4db302af2d3c007c23a461c62d2348979102
Codeepanshi <codenest24@gmail.com>
2025-05-06 18:40:58 -0600

    Update footer.js

--- frontend/src/Components/footer.js
+++ frontend/src/Components/footer.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa'; 
 
 const footer = () => {
   return (
     <div>
       {/* Footer */}
       <footer className="bg-black text-center text-sm text-white py-8 mt-12">
-
         {/* Copyright Text */}
         <p className="text-gray-200">&copy; {new Date().getFullYear()} Dr. Akshita's Dental Camp. All rights reserved.</p>
       </footer>
