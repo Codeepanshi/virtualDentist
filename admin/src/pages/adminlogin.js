988861f550f27da5f9b9ae4394bfce33d0811a77
Codeepanshi <codenest24@gmail.com>
2025-05-06 18:35:56 -0600

    Update adminlogin.js

--- admin/src/pages/adminlogin.js
+++ admin/src/pages/adminlogin.js
@@ -14,7 +14,7 @@ const AdminLogin = ({ setToken }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     
-    const res = await fetch('http://localhost:5000/api/auth/login', {
+    const res = await fetch('https://virtualdentist-server.onrender.com/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(form),
